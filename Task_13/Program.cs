1fe1110e83af70b4c6509fa24469139efd734a4e
Alexey <alexh2@yandex.ru>
2022-08-10 13:52:56 +0300

    Task_13

--- Task_13/Program.cs
+++ Task_13/Program.cs
@@ -8,7 +8,7 @@ int anyNumber = Convert.ToInt32(Console.ReadLine());
 string anyNumberText = Convert.ToString(anyNumber);
 if (anyNumberText.Length > 2)
 {
-  Console.WriteLine("третья цифра: " + anyNumberText[2]);
+  Console.WriteLine("третья цифра " + anyNumberText[2]);
 }
 else {
   Console.WriteLine("третьей цифры нет");
