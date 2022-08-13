3e8d993a77e066afbd6aa9818fa732c3163f4749
Alexey <alexh2@yandex.ru>
2022-08-13 10:31:24 +0300

    Task_19

---
+++ Task_3/Program.cs
@@ -0,0 +1,13 @@
+﻿// Если введено положительное число, возводим его в квадрат
+
+Console.WriteLine("Введите число");
+int number = int.Parse(Console.ReadLine()); // number = Convert.ToInt32(Console.Readline());
+if (number >= 0)
+{
+    number *= number; // number = number * number
+    Console.WriteLine("Число возведенное в квадрат: " + number);
+}
+else
+{
+    Console.WriteLine("Число отрицательное");
+    }
