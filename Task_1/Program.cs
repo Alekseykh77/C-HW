c2c981871cdea17346c5466fc9266bf49d0c2798
Alexey <alexh2@yandex.ru>
2022-08-13 13:33:47 +0300

    Task_1

---
+++ Task_1/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine ("Введите цифру дня недели");
+int number = int.Parse(Console.ReadLine());
+if (number == 1)
+{
+    Console.Write("Это понедельник");
+}
+else if (number == 2)
+{
+    Console.Write("Это втроник");
+}
+else if (number == 3)
+{
+    Console.Write("Это среда");
+}
+else if (number == 4)
+{
+    Console.Write("Это четверг");
+}
+else if (number == 5)
+{
+    Console.Write("Это пятница");
+}
+else if (number == 6)
+{
+    Console.Write("Это суббота");
+}
+else if (number == 7)
+{
+    Console.Write("Это воскресенье");
+}
+else Console.Write("Такого дня не существует");
