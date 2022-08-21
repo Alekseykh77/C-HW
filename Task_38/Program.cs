0ffd55a9f27d06a1d23d88dc3d1731a988284e21
Alexey <alexh2@yandex.ru>
2022-08-21 19:34:15 +0300

    Task_38

--- Task_38/Program.cs
+++ Task_38/Program.cs
@@ -5,13 +5,29 @@
 double[] arrayRealNumbers = new double[5];
   for (int i = 0; i < arrayRealNumbers.Length; i++ )
   {
-    arrayRealNumbers[i] = new Random().Next(1, 100);
-    Console.Write(arrayRealNumbers[i] + " ");
+    arrayRealNumbers[i] = new Random().Next(0, 100);
   }
+
+void PrintArry(double[] coll)
+{
+  int count = coll.Length;
+  int index = 0;
+  Console.Write("[");
+  while(index < count)
+  {
+    Console.Write(coll[index]);
+    index++;
+    if (index < count)
+    {
+      Console.Write(" ");
+    }
+  }
+  Console.Write("]");
+} 
+PrintArry(arrayRealNumbers);
 
 double maxNumber = arrayRealNumbers[0];
 double minNumber = arrayRealNumbers[0];
-
   for (int i = 1; i < arrayRealNumbers.Length; i++)
   {
     if (maxNumber < arrayRealNumbers[i])
@@ -28,4 +44,4 @@ double minNumber = arrayRealNumbers[0];
 
   double decision = maxNumber - minNumber;
 
-  Console.WriteLine($"Разница между максимальным элементом {maxNumber} и минимальным {minNumber} элементом равна {decision}");
+  Console.WriteLine($"Разница между максимальным {maxNumber} и минимальным {minNumber} элементом, равна {decision}");
