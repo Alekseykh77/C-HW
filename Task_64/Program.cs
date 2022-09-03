a203ae59ce378e2b7b5a70c74e47bddb0742f8fe
Alexey <alexh2@yandex.ru>
2022-09-04 00:46:22 +0300

    Task_64

---
+++ Task_64/Program.cs
@@ -0,0 +1,16 @@
+﻿ Console.Write("Введите число M: ");
+                  int M = Convert.ToInt32(Console.ReadLine());
+ Console.Write("Введите число N: ");
+                 int N = Convert.ToInt32(Console.ReadLine());
+                    Console.Write(RecursMetod(M,N));
+
+static int RecursMetod(int M, int N)
+        {
+            if (M < N)
+            {
+                Console.Write(M + ", ");
+                M++;
+                RecursMetod(M, N);
+            }
+            return N;
+        }
