8abf21815471317adbb434c123ac93a7b3bc635d
Alexey <alexh2@yandex.ru>
2022-09-04 11:01:14 +0300

    Task_66

--- Task_66/Program.cs
+++ Task_66/Program.cs
@@ -3,11 +3,11 @@
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
 
-Console.Write("Введите начальное число M: ");
-int numberM = int.Parse(Console.ReadLine());
+Console.Write("Введите число M: ");
+int numberM = Convert.ToInt32(Console.ReadLine());
 
-Console.Write("Введите начальное число N: ");
-int numberN = int.Parse(Console.ReadLine());
+Console.Write("Введите число N: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
 
 ///Метод нахождения суммы натуральных элементов в промежутке от M до N
 void GapNumberSum (int numberM, int numberN, int sum)
